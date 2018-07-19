cd4db647955245a244b0ab4703bc15084a06fe98
Eduardo Bechtold <eebechtold@gmail.com>
2018-07-19 14:33:36 -0300

    Removed extra tabs

--- app/src/main/java/com/darkwoods/notes/database/NoteVModel.java
+++ app/src/main/java/com/darkwoods/notes/database/NoteVModel.java
@@ -8,9 +8,9 @@ public class NoteVModel extends ViewModel {
 
     private LiveData<Note> noteLiveData;
 
-        public NoteVModel(AppDatabase database, int noteID) {
-            noteLiveData = database.noteDao().loadNoteById(noteID);
-        }
+    public NoteVModel(AppDatabase database, int noteID) {
+    noteLiveData = database.noteDao().loadNoteById(noteID);
+    }
 
     public LiveData<Note> getNoteLiveData() {
         return noteLiveData;
